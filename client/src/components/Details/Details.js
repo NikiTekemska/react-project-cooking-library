7d3b4cf86412887007fb9fb2837a210f7f032bc2
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-09 09:21:10 +0300

    Update Details.js

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,17 +1,92 @@
-export const Details = ({
-    name,
-    image,
-    timePreparation,
-    ingredients,
-    preparation
-}) => {
-    return(
-        <div>
-            <h1>{name}</h1>
-            <img src={image} alt="img" />
-            <h2>Time preparation:{timePreparation}</h2>
-            <h2>Ingredients: {ingredients}</h2>
-            <h2>Preparation: {preparation}</h2>
-        </div>
+import { useEffect, useState, useContext } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import styles from './Details.module.css';
+
+import { useService } from '../../hooks/useService'
+import { recipeServiceFactory } from '../../services/recipeService';
+import { AuthContext } from "../../contexts/AuthContext";
+import * as commentService from '../../services/commentService';
+
+import { AddComment } from "./AddComment/AddComment";
+
+
+
+export const Details = () => {
+    const { userId, isAuthenticated, userEmail } = useContext(AuthContext);
+    const [currentRecipe, setCurrentRecipe] = useState({});
+    const recipeService = useService(recipeServiceFactory);
+    const { category, recipeId } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        Promise.all([
+            recipeService.getOne(recipeId),
+            commentService.getAll(recipeId),
+        ])
+            .then(([recipeData, commentsData]) => {
+                setCurrentRecipe({
+                    ...recipeData,
+                    comments: commentsData
+                });
+            })
+
+
+    }, [recipeId]);
+
+    const onCommentSubmit = async (values) => {
+        const response = await commentService.create(recipeId, values.comment);
+        setCurrentRecipe(state => ({
+            ...state,
+            comments: [...state.comments, response]
+        }));
+
+    }
+
+    const isOwner = currentRecipe._ownerId === userId;
+    const onDelete = async () => {
+
+        // const result = confirm(`Are ypu sure you want to delete this ?`)
+
+        recipeService.deleteOne(recipeId);
+
+        // delete from the state
+        navigate('/recipes');
+
+    }
+
+    return (
+        <section >
+            <div className={styles.Details}>
+
+                <div >
+                    <img src={currentRecipe.image} alt="img" />
+                    <h1>{currentRecipe.title}</h1>
+                    <span className="">Preparation time: {currentRecipe.timePreparation}</span>
+                    <p className="type">Ingridients: {currentRecipe.ingridients}</p>
+                    <span className="">Preparation: {currentRecipe.preparation}</span>
+                </div>
+
+
+                {isOwner && (
+                    <div className="buttons">
+                        <Link to={`/recipes/${category}/${recipeId}/edit`} className="button"><button>Edit</button> </Link>
+                        <button href="#" className="button" onClick={onDelete}>Delete</button>
+                    </div>)}
+
+                <div className="details-comments">
+                    <h2>Comments:</h2>
+                    <ul>
+                        {currentRecipe.comments && currentRecipe.comments.map(x => (
+                            <li key={x._id} className="comment">
+                                <p>{userEmail}: {x.comment}</p>
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+
+            </div>
+            {isAuthenticated && <AddComment onCommentSubmit={onCommentSubmit} />}
+        </section>
     )
 }
