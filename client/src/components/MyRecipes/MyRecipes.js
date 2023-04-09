50104d3043fdfb3748df0c0b54ad8025f2e90b71
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-09 09:21:38 +0300

    Create MyRecipes.js

---
+++ client/src/components/MyRecipes/MyRecipes.js
@@ -0,0 +1,30 @@
+import { useContext, useEffect, useState } from "react"
+import { useService } from "../../hooks/useService"
+import { recipeServiceFactory } from "../../services/recipeService"
+import { AuthContext } from "../../contexts/AuthContext";
+import styles from './MyRecipes.module.css';
+
+import { RecipeCard } from "../Catalog/RecipeCard/RecipeCard";
+
+export const MyRecipes = () =>{
+    const recipeService = useService(recipeServiceFactory);
+    const [mineRecipe,setMineRecipe] = useState([]);
+    const { userId } = useContext(AuthContext);
+    useEffect(()=>{
+        recipeService.getAllMine(userId)
+            .then(data => {
+                setMineRecipe(data);
+            })
+    },[userId]);
+    return(
+        <>
+            <div className={styles.MyRecipes}>
+                <h1>My Recipes</h1>
+                {/* <img className='' src='' alt="img" />
+                <h3>Best way to finish your meal.</h3> */}
+            </div>
+            <div>{mineRecipe.map(x => <RecipeCard key={x._id} {...x} />)}</div>
+
+        </>
+    )
+}
