526ce8bfec24e9c72eac18891272cedf5158a740
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-09 09:22:10 +0300

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,4 +1,15 @@
-import * as request from './requester';
+import { requestFactory } from './requester';
 
 const baseUrl = `http://localhost:3030/users`;
-export const login = (loginData) => request.post(`${baseUrl}/login`, loginData);
+
+export const authServiceFactory = (token) => {
+    const request = requestFactory(token);
+    return {
+        login: (loginData) => request.post(`${baseUrl}/login`, loginData),
+
+        register: (registerData) => request.post(`${baseUrl}/register`, registerData),
+
+        logout: () => request.get(`${baseUrl}/logout`)
+
+    }
+}
