246fbd2edec067d3eec9c71cfa8d4eaf547610dc
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-11 18:48:42 +0300

    Update commentService.js

--- client/src/services/commentService.js
+++ client/src/services/commentService.js
@@ -4,9 +4,11 @@ const baseUrl = 'http://localhost:3030/data/comments';
 const request = requestFactory();
 
 export const getAll = async (recipeId) => {
-    const query = encodeURIComponent(`recipeId="${recipeId}"`);
+    const searchQuery = encodeURIComponent(`recipeId="${recipeId}"`);
+    const relationQuery = encodeURIComponent(`author=_ownerId:users`);
 
-    const result = await request.get(`${baseUrl}?where=${query}`);
+    const result = await request.get(`${baseUrl}?where=${searchQuery}&load=${relationQuery}`);
+
 
     const comments = Object.values(result);
 
