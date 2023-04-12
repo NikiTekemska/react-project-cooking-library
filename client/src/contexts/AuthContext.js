b3b762e0cc6ae8395890ebda94873c1eeaf79ec8
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-12 21:24:08 +0300

    Update AuthContext.js
    logout fixed

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext} from "react";
+import { createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
@@ -7,66 +7,75 @@ import { authServiceFactory } from "../services/authService";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({
-    children,
+  children,
 }) => {
-    const [auth,setAuth] = useLocalStorage('auth',{});
-    const navigate = useNavigate();
+  const [auth, setAuth] = useLocalStorage('auth', {});
+  const navigate = useNavigate();
 
-    const authService = authServiceFactory(auth.accessToken);
+  const authService = authServiceFactory(auth.accessToken);
 
-    const onLoginSubmit = async (data) => {
-        try {
-          const result = await authService.login(data);
-          setAuth(result);
-         
-          navigate('/');
-    
-        } catch (error) {
-          console.log(`Failed to login`);
-        }
-        
-    
-      };
-    
-      const onRegisterSubmit = async (values) => {
-        const { repeatPassword, ...registerData } = values;
-    
-        if (repeatPassword !== registerData.password) {
-          // alert('Repeat password doesn\'t match!')
-          return;
-        }
-        try {
-          const result = await authService.register(registerData);
-    
-          setAuth(result);
-          navigate('/');
-    
-        } catch (error) {
-          console.log('The Registration failed!')
-        }
-      };
-    
-      const onLogout = async() => {
-        await authService.logout();
-        
-        setAuth({}); // logout only on the client-session
-      };
+  const onLoginSubmit = async (data) => {
+    if (!data.email || !data.password) {
 
-      const context = {
-        onLoginSubmit,
-        onRegisterSubmit,
-        onLogout,
-        userId: auth._id,
-        token: auth.accessToken,
-        userEmail: auth.email,
-        isAuthenticated: !!auth.accessToken
-    
-      };
-    return (
-        <>
-        <AuthContext.Provider value={context}>
-            {children}
-        </AuthContext.Provider>
-        </>
-    )
+      alert('No empty fields allowed!');
+
+    } else {
+
+      try {
+        const result = await authService.login(data);
+        setAuth(result);
+
+        navigate('/');
+
+      } catch (error) {
+        alert("Wrong username or password");
+        console.log(`Failed to login`);
+      }
+    }
+  };
+
+  const onRegisterSubmit = async (values) => {
+    const { repeatPassword, ...registerData } = values;
+
+    if (repeatPassword !== registerData.password) {
+      // alert('Repeat password doesn\'t match!')
+      return;
+    }
+    try {
+      const result = await authService.register(registerData);
+
+      setAuth(result);
+      navigate('/');
+
+    } catch (error) {
+      console.log('The Registration failed!')
+    }
+  };
+
+  const onLogout = async () => {
+    try {
+      await authService.logout();
+    } catch(error) {
+      console.log(error)
+    }
+    setAuth({});
+  };
+
+  const context = {
+    onLoginSubmit,
+    onRegisterSubmit,
+    onLogout,
+    userId: auth._id,
+    token: auth.accessToken,
+    userEmail: auth.email,
+    isAuthenticated: !!auth.accessToken
+
+  };
+  return (
+    <>
+      <AuthContext.Provider value={context}>
+        {children}
+      </AuthContext.Provider>
+    </>
+  )
 }
