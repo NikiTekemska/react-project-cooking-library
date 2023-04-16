c0d06660dc4da65e97ec0251a8dd6745d2122ead
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-16 23:28:33 +0300

    Update MyRecipes.js

--- client/src/components/MyRecipes/MyRecipes.js
+++ client/src/components/MyRecipes/MyRecipes.js
@@ -6,24 +6,26 @@ import styles from './MyRecipes.module.css';
 
 import { RecipeCard } from "../Catalog/RecipeCard/RecipeCard";
 
-export const MyRecipes = () =>{
+export const MyRecipes = () => {
     const recipeService = useService(recipeServiceFactory);
-    const [mineRecipe,setMineRecipe] = useState([]);
+    const [mineRecipe, setMineRecipe] = useState([]);
     const { userId } = useContext(AuthContext);
-    useEffect(()=>{
+    useEffect(() => {
         recipeService.getAllMine(userId)
             .then(data => {
                 setMineRecipe(data);
             })
-    },[userId]);
-    return(
+    }, [userId]);
+    return (
         <>
             <div className={styles.MyRecipes}>
                 <h1>My Recipes</h1>
                 {/* <img className='' src='' alt="img" />
                 <h3>Best way to finish your meal.</h3> */}
             </div>
-            <div>{mineRecipe.map(x => <RecipeCard key={x._id} {...x} />)}</div>
+            {(mineRecipe.length === 0) ? <p className={styles.noRecipe}>No recipes yet!</p> :
+                <div>{mineRecipe.map(x => <RecipeCard key={x._id} {...x} />)}</div>
+            }
 
         </>
     )
