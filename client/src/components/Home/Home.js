888cbb61e7d9ce586e8ea824225ed950e04a5933
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-16 23:32:32 +0300

    Update Home.js

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -3,59 +3,57 @@ import { Link } from 'react-router-dom';
 export const Home = () => {
     return (
         <>
-            <section className={styles.welcome}>
-                <h1>Suitable for all tastes</h1>
-                {/* <button>LEARN MORE</button> */}
-            </section>
+
             <main className={styles.main}>
                 <section className={styles.welcome}>
-                    <h2>Welcome to Cooking library</h2>
-                    <p>If you have ever heard some of the questions 'What are we going to eat?' 'Do we have anyting for dessert?' 'Are we going to eat these again?'
+                    <h1>Suitable for all tastes</h1>
+
+                </section>
+                <section className={styles.welcomeMessage}>
+                    <p>If you have ever heard some of the questions: 'What are we going to eat?' 'Do we have anyting for dessert?' 'Are we going to eat this again?'
                         then you are in the right place!
-                    </p>
-                    <p>
                     </p>
                 </section>
                 <section className={styles.recipes}>
                     <h2>Recipes</h2>
                     <ul>
 
-                        <li><Link to="#">
+                        <li><Link to="/recipes/Salads">
                             <img src="./Images/backpack.png" alt="pic" />
                             <h3>Salads</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                         </Link></li>
-                        <li><a href="#">
+                        <li><Link to="/recipes/Desserts">
                             <img src="./Images/books.png" alt="pic" />
                             <h3>Desserts</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        </a></li>
-                        <li><a href="#">
+                        </Link></li>
+                        <li><Link to="/recipes/Pizza&Pasta">
                             <img src="./pizzapasta.jpg" alt="pic" />
                             <h3>Pizza & Pasta</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        </a></li>
-                        <li><a href="#">
+                        </Link></li>
+                        <li><Link to="/recipes/Meat&BBQ">
                             <img src="./Images/sandwich.png" alt="pic" />
-                            <h3>Meat</h3>
+                            <h3>Meat&BBQ</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        </a></li>
-                        <li><a href="#">
+                        </Link></li>
+                        <li><Link to="/recipes/Vegan">
                             <img src="./Images/first-aid-kit.png" alt="pic" />
                             <h3>Vegan</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        </a></li>
-                        <li><a href="#">
+                        </Link></li>
+                        <li><Link to="/recipes/Fish&chips">
                             <img src="./Images/plan.png" alt="pic" />
-                            <h3>Games and Аctivities</h3>
+                            <h3>Fish&Chips</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
-                        </a></li>
+                        </Link></li>
                     </ul>
                 </section>
 
