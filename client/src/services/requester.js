ccff19a29f8f96ebbfda28f3e58d55a500986ed6
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-17 22:04:13 +0300

    Update requester.js

--- client/src/services/requester.js
+++ client/src/services/requester.js
@@ -1,50 +1,51 @@
-
-const request = async (method, url, data) => {
+const requester = async (method, url, data) => {
   const options = {};
 
   if (method !== 'GET') {
-    options.method = method;
+      options.method = method;
 
-    if (data) {
-      options.headers = {
-        'content-type': 'application/json',
-      };
-      options.body = JSON.stringify(data);
-    };
+      if (data) {
+          options.headers = {
+              'content-type': 'application/json',
+          };
+
+          options.body = JSON.stringify(data);
+      }
   }
+
   const serializedAuth = localStorage.getItem('auth');
   if (serializedAuth) {
-    const auth = JSON.parse(serializedAuth);
-
-    if (auth.accessToken) {
-      options.headers = {
-        ...options.headers,
-        'X-Authorization': auth.accessToken,
-      };
-    }
+      const auth = JSON.parse(serializedAuth);
+      
+      if (auth.accessToken) {
+          options.headers = {
+              ...options.headers,
+              'X-Authorization': auth.accessToken,
+          };
+      }
   }
-  const response = await fetch(url, options);
 
+  const response = await fetch(url, options);
+  
 
   if (response.status === 204) {
-    return {};
+      return {};
   }
 
   const result = await response.json();
 
   if (!response.ok) {
-    throw result;
+      throw result;
   }
 
   return result;
 };
 
 export const requestFactory = () => {
-
   return {
-    get: request.bind(null, 'GET'),
-    put: request.bind(null, 'PUT'),
-    post: request.bind(null, 'POST'),
-    del: request.bind(null, 'DELETE'),
+      get: requester.bind(null, 'GET'),
+      post: requester.bind(null, 'POST'),
+      put: requester.bind(null, 'PUT'),
+      del: requester.bind(null, 'DELETE'),
   }
-}
+};
