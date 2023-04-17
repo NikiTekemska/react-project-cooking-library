91019e27b7e0580c54bdb0ca693cc27588dcac69
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-17 22:03:50 +0300

    Update SearchedRecipes.js

--- client/src/components/Header/SearchedRecipes.js
+++ client/src/components/Header/SearchedRecipes.js
@@ -1,13 +1,15 @@
 import { useContext } from "react";
 import { RecipeCard } from "../Catalog/RecipeCard/RecipeCard";
 import { RecipeContext } from "../../contexts/RecipesContext";
+import styles from './SearchedRecipes.module.css';
+
 export const SearchedRecipes = () => {
     const { matches } = useContext(RecipeContext);
 
     return (
         <>
             {(matches.length === 0) ?
-                <p> No matches found.</p>
+                <p className={styles.Matches}> No matches found.</p>
                 : <div>{matches.map((match) => <RecipeCard key={match._id} {...match} />)}</div>
             }
         </>
