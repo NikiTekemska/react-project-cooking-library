e1e8677444d2ab58c5185f66d1e69fec074ef1cf
Nikoleta Tekemska <nikoletatt94@gmail.com>
2023-04-17 22:19:01 +0300

    Update AuthContext.js

--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext} from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 
